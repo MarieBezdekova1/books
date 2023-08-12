4adebf8ca7e914082e30317c68bdb74ea54d9533
bezdekovam <marie.zachovalova@gmail.com>
2023-08-12 16:11:45 +0200

    pridano MappingConstants.java

---
+++ src/main/java/com/bezdekova/bookstore/constant/MappingConstants.java
@@ -0,0 +1,9 @@
+package com.bezdekova.bookstore.constant;
+
+public class MappingConstants {
+
+    public static final String API_V1 = "api/v1";
+    public static final String AUTHORS = API_V1 + "/authors";
+    public static final String AUTHORS_ONLY = AUTHORS + "Only";
+    public static final String BOOKS = API_V1 + "/books";
+}
